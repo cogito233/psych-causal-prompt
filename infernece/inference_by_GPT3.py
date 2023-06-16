973c40025d036d792b0218de72f0b20e6f0f84ff
ZHIHENG LYU <37894651+cogito233@users.noreply.github.com>
2023-06-16 03:21:11 -0700

    Update inference_by_GPT3.py

--- infernece/inference_by_GPT3.py
+++ infernece/inference_by_GPT3.py
@@ -16,7 +16,7 @@ if __name__=='__main__':
     for i in range(len(perfix_list)):
         tokenizer_list.append(GPT3TokenizerWarper([perfix_list[i], postfix_list[i]]))
 
-    model = GPT3Warper("/cluster/home/zzhiheng/cogito/openai_key/openai_key2.txt", f"gpt3_result_setup_{setup}_short_small_train.jsonl")
+    model = GPT3Warper("/path2OpenAIKey", f"gpt3_result_setup_{setup}_short_small_train.jsonl")
 
     dataset = load_from_disk('./yelp_small_train_new')
     #ic(dataset[:]['text'])
